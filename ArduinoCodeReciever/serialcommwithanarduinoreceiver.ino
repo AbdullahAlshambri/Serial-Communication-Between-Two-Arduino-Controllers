f1e16a2f2ed1a7865955d035fd634c1e63f4429a
AbdullahAlshambri <83937586+AbdullahAlshambri@users.noreply.github.com>
2021-07-11 16:50:05 +0300

    Update serialcommwithanarduinoreceiver.ino

--- ArduinoCodeReciever/serialcommwithanarduinoreceiver.ino
+++ ArduinoCodeReciever/serialcommwithanarduinoreceiver.ino
@@ -1,11 +1,12 @@
-char mystr[100] = "https://www.s-m.com.sa/"; //String data
+char mystr[100]; //Initialized variable to store recieved data
 
 void setup() {
-  
+  // Begin the Serial at 9600 Baud
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.write(mystr,100); 
+  Serial.readBytes(mystr,100); 
+  Serial.println(mystr); 
   delay(60000);
 }
